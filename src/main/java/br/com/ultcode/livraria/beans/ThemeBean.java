2d59c3fdc52b3531eb5ef33695aae1cbb6b7a03f
Geraldo <geraldocoosta@gmail.com>
2019-03-20 00:15:58 -0300

    Adicionando themas as nossa paginas

---
+++ src/main/java/br/com/ultcode/livraria/beans/ThemeBean.java
@@ -0,0 +1,27 @@
+package br.com.ultcode.livraria.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class ThemeBean {
+
+	private String theme = "aristo";
+
+	public String getTheme() {
+		return theme;
+	}
+
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+
+	public String[] getTemas() {
+		return new String[] { "afterdark", "afternoon", "afterwork", "aristo", "black-tie", "blitzer", "bluesky",
+				"bootstrap", "casablanca", "cupertino", "cruze", "dark-hive", "delta", "dot-luv", "eggplant",
+				"excite-bike", "flick", "glass-x", "home", "hot-sneaks", "humanity", "le-frog", "midnight", "mint-choc",
+				"overcast", "pepper-grinder", "redmond", "rocket", "sam", "smoothness", "south-street", "start",
+				"sunny", "swanky-purse", "trontastic", "ui-darkness", "ui-lightness", "vader" };
+	}
+}
